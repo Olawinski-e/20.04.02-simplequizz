15d6fc8e326653044070445a701641521ddc10e9
Estelle Mac <estelleolawinski@MacBook-Pro-de-Estelle-2.local>
2020-04-02 18:31:44 +0900

    having troubles with rendering

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ var quizContainer = document.getElementById("quiz");
 var resultsContainer = document.getElementById("results");
 var submitButton = document.getElementById("submit");
 
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
 function generateQuiz(
   questions,
   quizContainer,
@@ -30,15 +32,18 @@ function generateQuiz(
   submitButton
 ) {
   function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
     var output = [];
     var answers;
 
-    for (var i = 0; (i = questions.length); i++) {
-      //reset list of answers
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+      // first reset the list of answers
       answers = [];
-      // for each available answer
+
+      // for each available answer...
       for (letter in questions[i].answers) {
-        //add html radio button
+        // ...add an html radio button
         answers.push(
           "<label>" +
             '<input type="radio" name="question' +
@@ -46,12 +51,14 @@ function generateQuiz(
             '" value="' +
             letter +
             '">' +
+            letter +
+            ": " +
             questions[i].answers[letter] +
             "</label>"
         );
       }
 
-      //add question & answers to the output
+      // add this question and its answers to the output
       output.push(
         '<div class="question">' +
           questions[i].question +
@@ -62,47 +69,50 @@ function generateQuiz(
       );
     }
 
-    //finally combine output list to one string
-    quizContainer.innerHTML = output.join("");
+    // finally combine our output list into one string of html and put it on the page
+    //quizContainer.innerHTML = output.join("");
   }
 
   function showResults(questions, quizContainer, resultsContainer) {
+    // gather answer containers from our quiz
     var answerContainers = quizContainer.querySelectorAll(".answers");
 
-    //keep track of answers
+    // keep track of user's answers
     var userAnswer = "";
     var numCorrect = 0;
 
-    //for each question
+    // for each question...
     for (var i = 0; i < questions.length; i++) {
-      //find selected answer
+      // find selected answer
       userAnswer = (
         answerContainers[i].querySelector(
           "input[name=question" + i + "]:checked"
         ) || {}
       ).value;
 
-      //if answer is correct
+      // if answer is correct
       if (userAnswer === questions[i].correctAnswer) {
-        // add number to correct answers
+        // add to the number of correct answers
         numCorrect++;
 
-        //color answer green
+        // color the answers green
         answerContainers[i].style.color = "lightgreen";
       }
-
-      //if answer is not correct or blank
+      // if answer is wrong or blank
       else {
-        // color answer red
+        // color the answers red
         answerContainers[i].style.color = "red";
       }
     }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
   }
 
-  // show the questions
+  // show questions right away
   showQuestions(questions, quizContainer);
 
-  // when user clicks submit, show results
+  // on submit, show results
   submitButton.onclick = function() {
     showResults(questions, quizContainer, resultsContainer);
   };
